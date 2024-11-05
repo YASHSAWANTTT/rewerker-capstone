9c57a9c74289b7e5be5734fff543808fb17a81b3
Yash Sawant <137981875+YASHSAWANTTT@users.noreply.github.com>
2024-11-05 13:38:36 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,6 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-gray-700 via-gray-900 to-black">
         <img src="/image.png" width="500" height="500" alt="logo" />
-      
-      <h1 className="text-4xl font-bold mb-4">
-       NowhereCollective
-      </h1>
       <Link
         href="/access"
         className="px-6 py-3 bg-blue-500 text-white rounded-lg"
