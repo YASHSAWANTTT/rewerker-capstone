b40a8040d017ded60b8adf67dc77081f11df0b8c
Yash Sawant <137981875+YASHSAWANTTT@users.noreply.github.com>
2024-11-11 19:24:46 -0700

    favicon update

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
 import Link from "next/link";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-gray-700 via-gray-900 to-black">
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-gray-700 via-gray-900 to-black">
         <img src="/image.png" width="500" height="500" alt="logo" />
-      <Link
-        href="/access"
-        className="px-6 py-3 bg-blue-500 text-white rounded-lg"
-      >
-        Enter Access Code
-      </Link>
-    </div>
+        <Link
+          href="/access"
+          className="px-6 py-3 bg-blue-500 text-white rounded-lg"
+        >
+          Enter Access Code
+        </Link>
+      </div>
+    </>
   );
 }
